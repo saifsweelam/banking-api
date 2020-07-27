f6d1a3c102047dee5406747666450163853a4c6b
Seif El-Din Sweilam <saifsweelam@gmail.com>
2020-07-27 21:45:53 +0200

    select roles

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 """
-
+(Ayman)
 GET All Accounts
 GET All Transactions
+
+
+(Ahmed)
 GET Transactions by Account ID
-
 POST Acount
-POST Transaction (Account Balance Must be > 0)
 
+
+(Mohamed Zaitoon)
+POST Transaction (Account Balance Must be > 0)
 PATCH Account
 
 """
