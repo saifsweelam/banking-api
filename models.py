ba883350ec35d7bfcdbe633f9894e32dbcdcedc7
Peter maged <Peter.Maged@z2data.com>
2020-07-27 22:11:32 +0200

    added Transaction Class

--- models.py
+++ models.py
@@ -9,6 +9,15 @@ def create_models(app):
     class Account(db.Model):
     
     class Transaction(db.Model):
+    __tablename__ = 'transactions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    account_id= db.Column(db.Integer,db.ForeignKey('accounts.id'), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    type = db.Column(db.String(10))
+    date = db.Column(db.DateTime, nullable=False)
+    def __repr__(self):
+      return f'<Transaction {self.id} {self.acount_id} -  {self.amount}:{self.type}> '
 
     return {
         'db': db
