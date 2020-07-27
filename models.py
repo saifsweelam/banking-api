cb7ed94c2174f5bd3d0e40c669837b6b0688b758
Seif El-Din Sweilam <saifsweelam@gmail.com>
2020-07-27 22:54:22 +0200

    Add Account Model

--- models.py
+++ models.py
@@ -3,25 +3,44 @@ from flask_sqlalchemy import SQLAlchemy
 
 def create_models(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banking.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db = SQLAlchemy(app)
 
     class Account(db.Model):
+        __tablename__ = 'accounts'
+
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(64), nullable=False)
+        type = db.Column(db.String(32), nullable=False)
+        address = db.Column(db.String(100))
+        phone = db.Column(db.String(32))
+        balance = db.Column(db.Float, nullable=False)
+        active = db.Column(db.Boolean, default=True)
+
+        transactions = db.relationship('Transaction')
+
+        def __repr__(self):
+            return f'<Account {self.id} {self.name} -  {self.balance}:{self.type}> '
+
     
     class Transaction(db.Model):
-    __tablename__ = 'transactions'
+        __tablename__ = 'transactions'
 
-    id = db.Column(db.Integer, primary_key=True)
-    account_id= db.Column(db.Integer,db.ForeignKey('accounts.id'), nullable=False)
-    amount = db.Column(db.Float, nullable=False)
-    type = db.Column(db.String(10))
-    date = db.Column(db.DateTime, nullable=False)
-    def __repr__(self):
-      return f'<Transaction {self.id} {self.account_id} -  {self.amount}:{self.type}> '
+        id = db.Column(db.Integer, primary_key=True)
+        account_id= db.Column(db.Integer,db.ForeignKey('accounts.id'), nullable=False)
+        amount = db.Column(db.Float, nullable=False)
+        type = db.Column(db.String(10))
+        date = db.Column(db.DateTime, nullable=False)
+
+        def __repr__(self):
+            return f'<Transaction {self.id} {self.account_id} -  {self.amount}:{self.type}> '
+    
+    db.create_all()
 
     return {
-        'db': db
-        'Account': Account
+        'db': db,
+        'Account': Account,
         'Transaction': Transaction
     }
 
